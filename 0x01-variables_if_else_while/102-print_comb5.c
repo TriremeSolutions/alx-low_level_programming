289ca4ca1a1652d8ab91a16c849ca629b8da288d
TriremeSolutions <themellowread@gmail.com>
2022-12-09 23:43:20 -0800

    adv 3 betty

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of two 2-digit numbers from 00 to 99.
+ * main - Prints all possible combinations of two 2-digit numbers.
  *
  * Return: Always 0 (Success)
  */
